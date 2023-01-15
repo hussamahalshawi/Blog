8f29c2b8e05e4d6a62e56d824f8939da973e1616
hussamahalshawi <hussamahalshawi@gmail.com>
2023-01-15 19:52:08 +0200

    add Authentication and Authorization in project

--- core/views.py
+++ core/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from core.models import Blog
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import user_passes_test
 
 # Create your views here.
 def listing(request):
@@ -14,3 +16,31 @@ def view_blog(request, blog_id):
         "blog": blog,
     } 
     return render(request, "view_blog.html", data)
+def see_request(request):
+    text = f"""
+        Some attributes of the HttpRequest object:
+        scheme: {request.scheme}
+        path: {request.path}
+        method: {request.method}
+        GET: {request.GET}
+        user: {request.user}
+    """
+    return HttpResponse(text, content_type="text/plain")
+def user_info(request):
+    text = f"""
+        Selected HttpRequest.user attributes:
+        username: {request.user.username}
+        is_anonymous: {request.user.is_anonymous}
+        is_staff: {request.user.is_staff}
+        is_superuser: {request.user.is_superuser}
+        is_active: {request.user.is_active}
+    """
+    return HttpResponse(text, content_type="text/plain")
+
+@login_required
+def private_place(request):
+    return HttpResponse("members only!", content_type="text/plain")
+
+@user_passes_test(lambda user: user.is_staff)
+def staff_place(request):
+    return HttpResponse("Employees Only", content_type="text/plain")
